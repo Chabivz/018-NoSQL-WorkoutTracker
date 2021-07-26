4f60c1c74403836f74fb249a3659f67b8251fa03
Chris Abiva <chrisabiva@hotmail.com>
2021-07-26 14:53:05 -0700

    Updating the server

--- server.js
+++ server.js
@@ -12,9 +12,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 });
 
 // routes
